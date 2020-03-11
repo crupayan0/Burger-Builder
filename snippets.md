abd35ff043a73c5d2420feadd115000034611258
Rupayan <rupayanc16@gmail.com>
2020-03-11 21:14:04 +0530

    initial commit

---
+++ snippets.md
@@ -0,0 +1,4 @@
+rce -> React, Class, Export
+rcep -> React, Class, Export, Prop Types
+
+rafce -> React, Arrow func.comp., Export
---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+	{ label: 'Salad', type: 'salad' },
+	{ label: 'Bacon', type: 'bacon' },
+	{ label: 'Meat', type: 'meat' },
+	{ label: 'Cheese', type: 'cheese' }
+];
+
+const buildControls = props => {
+	return (
+		<div className={styles.BuildControls}>
+			<p>Total price: ${props.price.toFixed(2)}</p>
+			{controls.map(ctrl => (
+				<BuildControl
+					key={ctrl.label}
+					label={ctrl.label}
+					added={() => props.ingredientAdded(ctrl.type)}
+					removed={() => props.ingredientRemoved(ctrl.type)}
+					disabled={props.disabled[ctrl.type]}
+				/>
+			))}
+			<button
+				className={styles.OrderButton}
+				disabled={!props.purchasable}
+				onClick={props.ordered}
+			>
+				ORDER NOW
+			</button>
+		</div>
+	);
+};
+
+export default buildControls;
