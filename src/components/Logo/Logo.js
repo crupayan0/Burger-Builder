14835a29936587351b4cd464a2589e3cc5e3bab7
Rupayan <rupayanc16@gmail.com>
2020-03-12 13:54:02 +0530

    Fixed fetch bug of Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import styles from './Logo.module.css'
+import styles from './Logo.module.css';
+import Logo from '../../assets/original.png';
+
 const logo = () => {
 	return (
-        <img className={styles.Logo} src='https://a.udemycdn.com/2017-10-20_06-40-08-16f3842c99b1f93a100405760e522dd7/original.png?nva=20200311211957&token=0813eb2c58e8464106b8c' alt="Logo"></img>
+		<img
+			className={styles.Logo}
+			src={Logo}
+			alt='Logo'
+		></img>
 	);
 };
 
